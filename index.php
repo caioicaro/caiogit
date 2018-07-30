dbc997b2a1094b1daf00253117ef94756db72f81
caioicaro <caioicaro21@hotmail.com>
2018-07-30 20:49:35 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>teste</title>
+    </head>
+    <body>
+        <?php
+            echo "teste";
+            phpinfo();
+        ?>
+    </body>
+</html>
