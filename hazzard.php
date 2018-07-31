09679bf94ea9503453110e2666cc05e808492f09
caioicaro <caioicaro21@hotmail.com>
2018-07-31 12:15:44 -0300

    nova ação url

--- hazzard.php
+++ hazzard.php
@@ -3,8 +3,10 @@
 	{
 		return redirect->route('home');
 	}
-	public function entrar()
+	public function entrar($url)
 	{
+		$ed = $url + 5;
 		return redirect->route('entrar');
 	}
+	return redirect->route('entrar');
 ?>
