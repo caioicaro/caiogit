ed48b3f18f9afdc5ae815fd74179ba068a3010e0
caioicaro <caioicaro21@hotmail.com>
2018-07-31 12:00:31 -0300

    bug da funçãoABC corrigido 2

--- hazzard.php
+++ hazzard.php
@@ -3,8 +3,10 @@
 	{
 		return redirect->route('home');
 	}
-	public function entrar()
+	public function entrar($url)
 	{
+		if($url>0) return FALSE;
 		return redirect->route('entrar');
 	}
+	return redirect->route('home');
 ?>
